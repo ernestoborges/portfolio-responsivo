b4427b80ec68ab0bcd2c658bb693140f9682a567
Ernesto Neto <ernestoborges1995@gmail.com>
2023-02-17 18:28:08 -0300

    feat: Added link to projects repository and live preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { IoLogoLinkedin, IoLogoGithub, IoIosMail } from "react-icons/io"
+import { IoLinkOutline } from "react-icons/io5"
 
 import './App.css'
 
@@ -8,28 +9,45 @@ function App() {
   const projects = [
     {
       title: "Pokedex",
-      img: "",
+      img: "pokedex",
+      repo: "https://github.com/ernestoborges/Pokedex",
+      live: "https://csb-yjyxj5.netlify.app",
       text: "Projeto com meus primeiros passos em React, onde eu não entendia direito qual era a ideia do framework. Usa basicamente states e prop drilling. Foi um processo bem legal e foi o motivo de me fezer querer aprender mais React."
     },
     {
       title: "Countries Rest API",
-      img: "",
+      repo: "https://github.com/ernestoborges/frontendMentor-12",
+      live: "https://csb-hz0799.netlify.app",
+      img: "rest-countries",
       text: "Um desafio do Frontend Mentor. Utiliza uma API para informar detalhes sobre os Países. Aprendi sobre React Router e Navegação."
     },
     {
       title: "  Monster Encouters",
-      img: "",
+      img: "dnd5e",
+      repo: "https://github.com/ernestoborges/dnd5e-encounter-calculator",
+      live: "https://csb-un44vw.netlify.app",
       text: "Uma calculadora de dificuldade de encontros contra mosntros do jogo Dungeons and Dragons 5ª edição. Consome uma API e realiza cálculos retirados do manual do mestre do jogo."
     },
     {
       title: "Formulário",
-      img: "",
+      img: "ms-form",
+      repo: "https://github.com/ernestoborges/sossego",
+      live: "https://chic-pika-3b70d8.netlify.app",
       text: "Um teste técnico que realizei. É um formulário de multiplas etapas que realiza validação dos campos e armazena os dados em um contexto."
     },
     {
       title: "Front-end Mentor",
-      img: "",
+      img: "fm-form",
+      repo: "https://github.com/ernestoborges/FM-multi-step-form",
+      live: "https://stirring-kringle-b10f65.netlify.app",
       text: "Outro desafio do Frontend mentor. Também é um formulário de multiplas etapas, porém nesse eu utilizei a biblioteca Formik para gerenciar o formulário. Além disso, foi meu primeiro projeto utilizando TypeScript"
+    },
+    {
+      title: "Meu PC",
+      img: "my-pc",
+      repo: "https://github.com/ernestoborges/portfolio-react",
+      live: "https://ernesto-pc.netlify.app",
+      text: "Esse projeto é uma brincadeira que disponibiliza informações do meu portfolio imitando um sistema operacional animado. É uma experiência bem divertida, porém é melhor quando acessado por dispositivos com telas em proporções de monitores."
     }
   ]
 
@@ -95,18 +113,22 @@ function App() {
                     <div className='border-gap border-right'></div>
                     <div className='border-gap border-bottom'></div>
                     <div className='border-gap border-left'></div>
-
                     <article key={index}>
                       <header>
                         <h3>
                           {project.title}
                         </h3>
+                        <section>
+                          <a href={project.live} title={`link do projeto: ${project.title}`} target="_blank"><IoLinkOutline />Live</a>
+                          <a href={project.repo} title={`link do repositorio: ${project.title}`} target="_blank"><IoLogoGithub />Github</a>
+                        </section>
                       </header>
                       <section>
-                        <img />
+                        <img src={project.img ? `images/${project.img}.png` : ""} />
                       </section>
                       <section>
                         <p>
+                          <span>&#62; </span>
                           {project.text}
                         </p>
                       </section>
